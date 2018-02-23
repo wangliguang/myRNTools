309b3d9b46b29a4275a74910760f3e34bcf4d94d
王立广 <wangliguang-rj@ofo.com>
2018-02-23 14:21:48 +0800

    feat: 优化注册路由

--- src/Navigation.js
+++ src/Navigation.js
@@ -1,3 +1,4 @@
+import React, { Component } from 'react';
 import HomePage from './HomePage';
 import GGTimerScene from './Scene/TimerScene';
 import GGLimitDoubleClickBtnScene from './Scene/LimitDoubleClickBtnScene';
@@ -19,22 +20,41 @@ import {
 initNetEventListener();
 
 // 注册导航
-const Navs = StackNavigator({
-  HomePage: { screen: HomePage, navigationOptions: ({navigation}) => StackOptions({navigation}) },
-  GGTimer: { screen: GGTimerScene, navigationOptions: ({navigation}) => StackOptions({navigation})},
-  GGLimitDoubleClickBtn: { screen: GGLimitDoubleClickBtnScene, navigationOptions: ({navigation}) => StackOptions({navigation}) },
-  Storage: { screen: StorageScene, navigationOptions: ({navigation}) => StackOptions({navigation})},
-  keyBoard: { screen: KeyBoardScene, navigationOptions: ({navigation}) => StackOptions({navigation})},
-  
-  TypeTab: { screen: TypeTabScene, navigationOptions: ({navigation}) => StackOptions({navigation})},
+const AppNavigator = StackNavigator({
+  HomePage: { screen: HomePage },
+  GGTimer: { screen: GGTimerScene },
+  GGLimitDoubleClickBtn: { screen: GGLimitDoubleClickBtnScene },
+  Storage: { screen: StorageScene },
+  keyBoard: { screen: KeyBoardScene },
+  TypeTab: { screen: TypeTabScene },
 }, {
   initialRouteName: 'HomePage', // 默认显示界面
   navigationOptions: { header: null },
   mode: 'card',  // 页面切换模式, 左右是card(相当于iOS中的push效果), 上下是modal(相当于iOS中的modal效果)
   headerMode: 'screen', // 导航栏的显示模式, screen: 有渐变透明效果, float: 无透明效果, none: 隐藏导航栏
+  onTransitionEnd: (() => {
+    const test = AppRouter.getComponentForRouteName('TypeTab');
+}),
 });
-StackOptions = (navigator) => {
-  registerNavigator(navigator.navigation);
-}
 
-export default Navs;
+const AppRouter = AppNavigator.router;
+
+class App extends React.Component {
+  
+  componentDidMount(){
+    registerNavigator(this.navigator._navigation);
+  }
+
+  componentWillUnmount() {
+    unRegisterNavigator();
+  }
+
+  render() {
+    return (
+      <AppNavigator
+        ref={nav => { this.navigator = nav; }}
+      />
+    );
+  }
+}
+export default App;
